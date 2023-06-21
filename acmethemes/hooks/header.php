bf3897a8a1aebea7f4eda07b4a2c709c3508ab27
Alison <alisonpinheiro17@gmail.com>
2023-06-21 19:56:47 -0300

    add menu mobile

--- acmethemes/hooks/header.php
+++ acmethemes/hooks/header.php
@@ -149,90 +149,104 @@ if (!function_exists('supermag_header')):
             supermag_header_markup();
         }
         ?>
-            <header id="masthead" class="site-header" role="banner">
-                <div class="header-wrapper container">
-                    <div class="header-container">
+        <header id="masthead" class="site-header" role="banner">
+            <div class="header-wrapper container">
+                <div class="header-container">
+                    <?php
+                    if ('above-logo' == $supermag_header_media_position) {
+                        supermag_header_markup();
+                    }
+                    ?>
+
+
+                    <?php if ('disable' != $supermag_customizer_all_values['supermag-header-id-display-opt']): ?>
+
                         <?php
-                        if ('above-logo' == $supermag_header_media_position) {
-                            supermag_header_markup();
-                        }
-                        ?>
-
-
-                        <?php if ('disable' != $supermag_customizer_all_values['supermag-header-id-display-opt']): ?>
-                            
-                                <?php
-                                if ('logo-only' == $supermag_customizer_all_values['supermag-header-id-display-opt']):
-                                    if (function_exists('the_custom_logo')):
-                                        the_custom_logo();
-                                    else:
-                                        if (!empty($supermag_customizer_all_values['supermag-header-logo'])):
-                                            $supermag_header_alt = get_bloginfo('name');
-                                            ?>
-                                            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                                                <img src="<?php echo esc_url($supermag_customizer_all_values['supermag-header-logo']); ?>"
-                                                    alt="<?php echo esc_attr($supermag_header_alt); ?>">
-                                            </a>
-                                            <?php
-                                        endif; /*supermag-header-logo*/
-                                    endif;
-                                else: /*else is title-only or title-and-tagline*/
-                                    if (is_front_page() && is_home()): ?>
-                                <h1 class="site-title">
-                                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-                                </h1>
-                            <?php else: ?>
-                                <p class="site-title">
-                                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-                                </p>
-                            <?php endif;
-                                    if ('title-and-tagline' == $supermag_customizer_all_values['supermag-header-id-display-opt']):
-                                        $description = get_bloginfo('description', 'display');
-                                        if ($description || is_customize_preview()): ?>
-                                <?php endif;
-                                    endif;
-                                endif; ?>
-                            <!--site-logo-->
+                        if ('logo-only' == $supermag_customizer_all_values['supermag-header-id-display-opt']):
+                            if (function_exists('the_custom_logo')):
+                                the_custom_logo();
+                            else:
+                                if (!empty($supermag_customizer_all_values['supermag-header-logo'])):
+                                    $supermag_header_alt = get_bloginfo('name');
+                                    ?>
+                                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                                        <img src="<?php echo esc_url($supermag_customizer_all_values['supermag-header-logo']); ?>"
+                                            alt="<?php echo esc_attr($supermag_header_alt); ?>">
+                                    </a>
+                                    <?php
+                                endif; /*supermag-header-logo*/
+                            endif;
+                        else: /*else is title-only or title-and-tagline*/
+                            if (is_front_page() && is_home()): ?>
+                        <h1 class="site-title">
+                            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+                        </h1>
+                    <?php else: ?>
+                        <p class="site-title">
+                            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+                        </p>
+                    <?php endif;
+                            if ('title-and-tagline' == $supermag_customizer_all_values['supermag-header-id-display-opt']):
+                                $description = get_bloginfo('description', 'display');
+                                if ($description || is_customize_preview()): ?>
                         <?php endif;
+                            endif;
+                        endif; ?>
+                        <!--site-logo-->
+                    <?php endif;
 
-                        ?>
+                    ?>
 
 
 
-                        <nav id="site-navigation" class="main-navigation " role="navigation">
-                            <div class="header-main-menu ">
-                                <?php
-                                wp_nav_menu(array('theme_location' => 'primary', 'container' => 'div', 'container_class' => 'acmethemes-nav'));
-                                if (!has_nav_menu('primary')) {
-                                    echo "</div>";
-                                }
-
-                                if (isset($supermag_customizer_all_values['supermag-menu-show-search']) && $supermag_customizer_all_values['supermag-menu-show-search'] == 1):
-
-                                    $supermag_menu_search_type = $supermag_customizer_all_values['supermag-menu-search-type'];
-                                    if ('dropdown-search' == $supermag_menu_search_type) {
-                                        echo '<a class="fa fa-search icon-menu search-icon-menu" href="#"></a>';
-                                        echo "<div class='menu-search-toggle'>";
-                                        echo "<div class='menu-search-inner'>";
-                                    }
-                                    get_search_form();
-
-                                    if ('dropdown-search' == $supermag_menu_search_type) {
-                                        echo '</div>'; /*menu-search-inner*/
-                                        echo '</div>'; /*menu-search-toggle*/
-                                    }
-                                endif;
-                                ?>
+                    <nav id="site-navigation" class="main-navigation " role="navigation">
+                        <div class="header-main-menu ">
+                            <?php
+                            wp_nav_menu(array('theme_location' => 'primary', 'container' => 'div', 'container_class' => 'acmethemes-nav'));
+                            if (!has_nav_menu('primary')) {
+                                echo " <button class='js-toggle-menu header-mobile__button header-mobile__button--close'>
+                                <svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' width='50' height='50' viewBox='0 0 50 50'>
+                                <path d='M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z'></path>
+                                </svg>
+                            </button>
+                            
                             </div>
-                            <div class="responsive-slick-menu wrapper clearfix"></div>
-                        </nav>
-                        <!-- #site-navigation -->
-                    </div>
-                    <!-- .header-container -->
+                            <div class='header-mobile__overlay js-toggle-menu'></div>";
+                            }
+
+                            if (isset($supermag_customizer_all_values['supermag-menu-show-search']) && $supermag_customizer_all_values['supermag-menu-show-search'] == 1):
+
+                                $supermag_menu_search_type = $supermag_customizer_all_values['supermag-menu-search-type'];
+                                if ('dropdown-search' == $supermag_menu_search_type) {
+                                    echo '<a class="fa fa-search icon-menu search-icon-menu" href="#"></a>';
+                                    echo "<div class='menu-search-toggle'>";
+                                    echo "<div class='menu-search-inner'>";
+                                }
+                                get_search_form();
+
+                                if ('dropdown-search' == $supermag_menu_search_type) {
+                                    echo '</div>'; /*menu-search-inner*/
+                                    echo '</div>'; /*menu-search-toggle*/
+                                }
+                            endif;
+                            ?>
+                            <button class="js-toggle-menu header-mobile__button">
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"
+                                    viewBox="0 0 24 24">
+                                    <path d="M2 11H22V13H2zM2 5H22V7H2zM2 17H22V19H2z"></path>
+                                </svg>
+                            </button>
+
+                        </div>
+
+                    </nav>
+                    <!-- #site-navigation -->
                 </div>
-                <!-- header-wrapper-->
-            </header>
-            <?php
+                <!-- .header-container -->
+            </div>
+            <!-- header-wrapper-->
+        </header>
+        <?php
     }
 endif;
 add_action('supermag_action_header', 'supermag_header', 10);
@@ -251,44 +265,44 @@ if (!function_exists('supermag_before_content')):
     function supermag_before_content()
     {
         ?>
-            <main class=" content-wrapper container">
-                <?php
-                $supermag_customizer_all_values = supermag_get_theme_options();
-                $supermag_enable_feature = $supermag_customizer_all_values['supermag-enable-feature'];
-                if (is_front_page() && 1 == $supermag_enable_feature) {
-                    echo '<div class="slider-feature-wrap clearfix">';
-                    /*Slide*/
-                    /**
-                     * supermag_action_feature_slider
-                     * @since SuperMag 1.1.0
-                     *
-                     * @hooked supermag_feature_slider -  0
-                     */
-                    do_action('supermag_action_feature_slider');
+        <main class=" content-wrapper container">
+            <?php
+            $supermag_customizer_all_values = supermag_get_theme_options();
+            $supermag_enable_feature = $supermag_customizer_all_values['supermag-enable-feature'];
+            if (is_front_page() && 1 == $supermag_enable_feature) {
+                echo '<div class="slider-feature-wrap clearfix">';
+                /*Slide*/
+                /**
+                 * supermag_action_feature_slider
+                 * @since SuperMag 1.1.0
+                 *
+                 * @hooked supermag_feature_slider -  0
+                 */
+                do_action('supermag_action_feature_slider');
 
-                    /*Featured Post Beside Slider*/
-                    /**
-                     * supermag_action_feature_side
-                     * @since SuperMag 1.1.0
-                     *
-                     * @hooked supermag_feature_side-  0
-                     */
-                    do_action('supermag_action_feature_side');
-                    echo "</div>";
-                    $supermag_content_id = "home-content";
-                } else {
-                    $supermag_content_id = "content";
+                /*Featured Post Beside Slider*/
+                /**
+                 * supermag_action_feature_side
+                 * @since SuperMag 1.1.0
+                 *
+                 * @hooked supermag_feature_side-  0
+                 */
+                do_action('supermag_action_feature_side');
+                echo "</div>";
+                $supermag_content_id = "home-content";
+            } else {
+                $supermag_content_id = "content";
+            }
+            ?>
+            <div id="<?php echo esc_attr($supermag_content_id); ?>" class="site-content">
+                <?php
+                $sidebar_layout = supermag_sidebar_selection(get_the_ID());
+                if ('both-sidebar' == $sidebar_layout) {
+                    echo '<div id="primary-wrap" class="clearfix">';
                 }
-                ?>
-                <div id="<?php echo esc_attr($supermag_content_id); ?>" class="site-content">
-                    <?php
-                    $sidebar_layout = supermag_sidebar_selection(get_the_ID());
-                    if ('both-sidebar' == $sidebar_layout) {
-                        echo '<div id="primary-wrap" class="clearfix">';
-                    }
-                    if (1 == $supermag_customizer_all_values['supermag-show-breadcrumb'] && !is_front_page()) {
-                        supermag_breadcrumbs();
-                    }
+                if (1 == $supermag_customizer_all_values['supermag-show-breadcrumb'] && !is_front_page()) {
+                    supermag_breadcrumbs();
+                }
     }
 endif;
 add_action('supermag_action_after_header', 'supermag_before_content', 10);
